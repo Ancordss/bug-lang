c6d49798a6ca5fd73ab994b63b54014331582b61
Javaraxi-17 <java.najera13@gmail.com>
2024-03-20 23:07:44 -0600

    diseño_bonito

--- buglang.py
+++ buglang.py
@@ -1,103 +1,50 @@
-#import docopt
-#from argparse import ArgumentParser
+import click
 from bug_lang.context import Context
-from rich import print
-from bug_lang.render import DotRender
-#from rich import print
+from rich.console import Console
+from rich.syntax import Syntax
 from tabulate import tabulate
 
-#TODO: use click instead of this
+# Define the command-line interface using Click decorators
+@click.command()
+@click.argument('input_file', type=click.File('r'))
+@click.option('-l', '--lex', is_flag=True, help='Display tokens from lexer')
+@click.option('-a', '--AST', is_flag=True, help='Display AST')
+@click.option('-D', '--dot', is_flag=True, help='Generate AST graph as DOT format')
+@click.option('--sym', is_flag=True, help='Dump the symbol table')  # the Checker one
+@click.option('-R', '--exec', 'execute', is_flag=True, help='Execute the generated program')
+def main(input_file, lex, ast, dot, sym, execute):
+    console = Console()
 
-"""
-    print("optional arguments:")
-    print("-h, --help             show this help message and exit")
-    print("-d, --debug            Generate assembly with extra information (for debugging purposes)")
-    print("-o OUT, --out OUT      File name to store generated executable")
-    print("-l, --lex              Store output of lexer")
-    print("-a, --AST              Display AST")
-    print("-D, --dot              Generate AST graph as DOT format")
-    print("-p, --png              Generate AST graph as png format")
-    print("-I, --ir               Dump the generated Intermediate representation")
-    print("--sym                  Dump the symbol table") #the Checker one
-    print("-S, --asm              Store the generated assembly file")
-    print("-R, --exec             Execute the generated program")
-"""
-def menu():
-    print("\t\t\t\n ################################ Bug-lang ################################  \n")
-    print("usage: buglang.py [-h] [-d] [-o OUT] [-l] [-a] [-D] [-p] [-I] [--sym] [-S] [-R] input\n")
-
-    print("Compiler for Bug-lang programs\n")
-
-    print("positional arguments:")
-    print("input Bug-lang            program file to compile\n")
-
-    print("optional arguments:")
-    print("-h, --help             show this help message and exit")
-    print("-l, --lex              display tokens from lexer")
-    print("-a, --AST              Display AST")
-    print("-D, --dot              Generate AST graph as DOT format")
-    #print("-p, --png              Generate AST graph as png format")
-    print("--sym                  Dump the symbol table") #the Checker one
-    print("-R, --exec             Execute the generated program")
-
-
-def main(argv):
-    if len(argv) == 2:
-        menu()
-        raise SystemExit()
-
-    print("\t\t\t\n ################################ Bug-lang Compiler ################################  \n")
+    console.print("\t\t\t\n[bold green]################################ Bug-lang Compiler ################################[/bold green]\n")
     ctxt = Context()
-    if len(argv) > 2:
-        source = ""
-        with open(argv[2]) as file:
-            source = file.read()
-        ctxt.parse(source)
-        if not ctxt.have_errors:
-            if argv[1] in ["-h","--help"]:
-                menu()
-                #raise SystemExit()
-            elif argv[1] in ["-l","--lex"]:
-                print("\n\n\t\t********** TOKENS ********** \n\n")
-                tokens = ctxt.lexer.tokenize(source)
-                table=[["Type","Value","At line"]]
-                for tok in tokens:
-                    row=[]
-                    row.append(tok.type)
-                    row.append(tok.value)
-                    row.append(tok.lineno)
-                    table.append(row)
-                print(tabulate(table, headers='firstrow', tablefmt='fancy_grid'))
-            elif argv[1] in ["-a","--AST"]:
-                print("\n\n\t\t********** AST ********** \n\n")
-                print(ctxt.ast)
-            elif argv[1] in ["-D","--dot"]:
-                print("\n\n DOT LANGUAGE \n")
-                dot = DotRender.render(ctxt.ast) #render
-                print(dot)
-            elif argv[1] in ["-s","--sym"]:
-                print(ctxt.interp.env)
-            elif argv[1] in ["-R", "--exec"]:
-                #print("\n CHECKER + INTERPRETER \n")
-                ctxt.run()
-            else:
-                print("Not defined action")
-                op = int(input("Do you need help? 1:yes/2:no :: "))
-                if op == 1:
-                    menu()
-    else:
-        try:
-            while True:
-                source = input("bug > ") #This one works for very simple one line stuff, but the environments of neither Checker or Interpret work properly.
-                ctxt.parse(source)
-                if ctxt.have_errors: continue
-                for stmt in ctxt.ast.decl:
-                    ctxt.ast = stmt
-                    ctxt.run()
+    source = input_file.read()
+    ctxt.parse(source)
 
-        except EOFError:
-            pass
+    if lex:
+        console.print("\n\n[bold blue]********** TOKENS **********[/bold blue]\n")
+        tokens = ctxt.lexer.tokenize(source)
+        table = [["Type", "Value", "At line"]]
+        for tok in tokens:
+            row = [tok.type, tok.value, tok.lineno]
+            table.append(row)
+        console.print(tabulate(table, headers='firstrow', tablefmt='fancy_grid'))
+
+    if ast:
+        console.print("\n\n[bold magenta]********** AST **********[/bold magenta]\n")
+        ast_syntax = Syntax(str(ctxt.ast), "python", theme="ansi_dark", line_numbers=True)
+        console.print(ast_syntax)
+
+    if dot:
+        console.print("\n\n[bold cyan]DOT LANGUAGE[/bold cyan]\n")
+        dot = DotRender.render(ctxt.ast)  # render
+        console.print(dot)
+
+    if sym:
+        console.print("\n[bold yellow]Symbol Table[/bold yellow]\n")
+        console.print(ctxt.interp.env)
+
+    if execute:
+        ctxt.run()
 
 if __name__ == "__main__":
-    from sys import argv
-    main(argv)
+    main()
